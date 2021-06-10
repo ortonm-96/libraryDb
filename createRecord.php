bc6251813fbbb9713d02e57e4c900a280776023f
ortonm-96 <molly.orton.96@gmail.com>
2021-06-10 16:06:53 +0100

    Update createRecord.php

--- createRecord.php
+++ createRecord.php
@@ -14,9 +14,15 @@
 	// Should I output the returned record?
 	// Feels like I ought to have some feedback here at the very least.
 	// Bare minimum a home button, some sort of "success" display, and something showing the created record
-	// Does statement->execute return the new row? I think it does
+	// Does statement->execute return the new row? I think it does. no it doesn't lol
 	// So i could pass that to another function to format it - that'll be a pretty handy thing to have in general
+
+	$created = $pdo->lastInsertId();
+	echo $created;
+
+
 
 ?>
 Item created <br>
+
 <a href="index.php">Home</a>
