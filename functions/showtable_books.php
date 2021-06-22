d1200f8ac53c45edec8074134f521bccd0e13080
ortonm-96 <molly.orton.96@gmail.com>
2021-06-22 12:39:20 +0100

    Added books on loan view

--- functions/showtable_books.php
+++ functions/showtable_books.php
@@ -12,5 +12,16 @@ function getBooks(){
 	return $tableOutput;
 }
 
+function getBooks_loaned(){
+	require_once "functions/config.php";
+
+	$sqlStatement = 'SELECT * FROM books WHERE loan_date IS NOT NULL';
+
+	$statement = $pdo->prepare($sqlStatement);
+	$statement->execute();
+	$tableOutput = $statement->fetchAll();
+
+	return $tableOutput;
+}
 
 ?>
---
+++ view_loaned_books.php
@@ -0,0 +1,22 @@
+<?php include "templates/bootstrapReqs.html";?>
+<?php include "templates/navbar.html";?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Loaned books</title>
+</head>
+<body>
+	<div class="container-fluid float-left border rounded">
+	<?php
+		require_once "functions/showtable_books.php";
+		require_once "functions/displayRecord.php";
+
+		$booksTable = getBooks_loaned();
+		$booksTableFormatted = formatRecordsAsTable($booksTable, "books", "isbn");
+		echo $booksTableFormatted;
+	?>
+	</div>
+</body>
+<?php include "templates/bootstrapScript.html";?>
+</html>
