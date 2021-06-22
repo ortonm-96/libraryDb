4e8f411b6cfc181049685e719ed3d9f61ccdc932
ortonm-96 <molly.orton.96@gmail.com>
2021-06-22 12:35:56 +0100

    Have loan/return set the user ID

--- functions/update.php
+++ functions/update.php
@@ -22,12 +22,13 @@ function postRequest_Update_Books($postRequest){
 function postRequest_Update_Books_setLoanDate($postRequest){
 	require_once "functions/config.php";
 	
-	$sqlStatement = "UPDATE books SET loan_date = :loan_date WHERE isbn = :isbn";
+	$sqlStatement = "UPDATE books SET loan_date = :loan_date, loaned_by_user_id = :loaned_by_user_id WHERE isbn = :isbn";
 
 	$statement = $pdo->prepare($sqlStatement);
 
 	$statement->execute([
 		'loan_date' => $postRequest["loan_date"],
+		'loaned_by_user_id' => $postRequest["loaned_by_user_id"],
 		'isbn' => $postRequest["isbn"]
 	]);
 	
