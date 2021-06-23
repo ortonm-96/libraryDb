20ff1412b4bc73786001d12be0091baf43b82725
ortonm-96 <molly.orton.96@gmail.com>
2021-06-23 16:28:35 +0100

    Proper templates for record pages

---
+++ recordPage_booksTemplate.php
@@ -0,0 +1,78 @@
+<?php
+	// Todo: this whole file is ugly, refactor it to read better
+	//echo $resultsFormatted
+	$id = $queryResults[0]["id"] ?: "";
+	$isbn = $queryResults[0]["isbn"] ?: "";
+	$name = $queryResults[0]["name"] ?: "";
+	$description = $queryResults[0]["description"] ?: "";
+	$loan_date = $queryResults[0]["loan_date"] ?: "";
+	$due_date = $queryResults[0]["due_date"] ?: "";
+	$loaned_by_user_id = $queryResults[0]["loaned_by_user_id"] ?: "";
+
+	if (!empty($loaned_by_user_id)){
+		$userInfo = getRequest_query(["table"=>"users", "user_id"=>$loaned_by_user_id]);
+		$userFullName = $userInfo[0]["full_name"];
+		$userRecordLink = "recordPage.php?table=users&user_id={$loaned_by_user_id}";
+	} else{
+		$userInfo = "";
+		$userFullName = "";
+		$userRecordLink = "";
+	}
+
+
+	if(!empty($loan_date)){
+			$loanDisabled = " disabled";
+			$returnDisabled = "";
+		} else {
+			$loanDisabled = "";
+			$returnDisabled = " disabled";
+		}
+
+?>
+<p>Book page template test</p>
+<form method="post">
+
+	<div class="form-group d-none">
+		<label for="first_name">ID</label>
+		<input required type="number" name="id" id="id" class="form-control" value=<?php echo "\"{$id}\""; ?>></input>
+	</div>
+
+	<div class="form-group">
+		<label for="isbn">ISBN</label>
+		<input required type="number" name="isbn" id="isbn" class="form-control" value=<?php echo "\"{$isbn}\""; ?>></input>
+	</div>
+
+	<div class="form-group">
+		<label for="name">Name</label>
+		<input type="text" name="name" id="name" class="form-control" value=<?php echo "\"{$name}\""; ?>></input>
+	</div>
+
+	<div class="form-group">
+		<label for="description">Description</label>
+		<input type="text" name="description" id="description" class="form-control" value=<?php echo "\"{$description}\""; ?>></input>
+	</div>
+
+	<div class="form-group">
+		<label for="loan_date">Loan Date</label>
+		<input type="date" name="loan_date" id="loan_date" class="form-control" value=<?php echo "\"{$loan_date}\""; ?>></input>
+	</div>
+
+	<div class="form-group">
+		<label for="due_date">Due Date</label>
+		<input disabled type="date" name="due_date" id="due_date" class="form-control" value=<?php echo "\"{$due_date}\""; ?>></input>
+	</div>
+
+	<div class="form-group">
+		<label for="loaned_by_user_id">Loaned By</label>
+		<!-- Hide the actual input, because there's no reason for the user to have access to this.-->
+		<input type="hidden" name="loaned_by_user_id" id="loaned_by_user_id" class="form-control" value=<?php echo "\"{$loaned_by_user_id}\""; ?>></input>
+		<!-- Generate a link to the user's record page if applicable -->
+		<a href=<?php echo "{$userRecordLink}"; ?>><?php echo "{$userFullName}"; ?></a>
+	</div>
+
+
+	<button type="submit" role="button" class="btn btn-light" formaction="updated_record.php">Update</button>
+	<button type="submit" role="button" class="btn btn-light" formaction="deleted_record.php">Delete</button>
+	<button type="submit" role="button" class="btn btn-light" formaction="loanBook.php"<?php echo $loanDisabled; ?>>Loan book</button>
+	<button type="submit" role="button" class="btn btn-light" formaction="returnBook.php"<?php echo $returnDisabled; ?>>Return book</button>
+</form>
---
+++ recordPage_userTemplate.php
@@ -0,0 +1,38 @@
+<?php
+require_once "functions/showtable_books.php";
+
+//echo $resultsFormatted
+$user_id = $queryResults[0]["user_id"] ?: "";
+$first_name = $queryResults[0]["first_name"] ?: "";
+$last_name = $queryResults[0]["last_name"] ?: "";
+$full_name = $queryResults[0]["full_name"] ?: "";
+?>
+<p>User page - <?php echo $full_name; ?></p>
+<form>
+
+	<div class="form-group">
+		<label for="first_name">First Name</label>
+		<input type="text" name="first_name" id="first_name" class="form-control" value=<?php echo "\"{$first_name}\""; ?>></input>
+	</div>
+
+	<div class="form-group">
+		<label for="last_name">Last Name</label>
+		<input type="text" name="last_name" id="last_name" class="form-control" value=<?php echo "\"{$last_name}\""; ?>></input>
+	</div>
+
+</form>
+
+<!-- Display books on loan - construct a query similar to , then pass to table render function -->
+<p>Books on loan</p>
+<?php
+	$userBooksOnLoan = getBooks_loaned_byUser($user_id);
+	$headerRow = [
+			["isbn", "ISBN"],
+			["name", "Name"],
+			["description", "Description"],
+			["full_name", "Loaned by"],
+			["due_date", "Due by"]
+		];
+	$userBooksOnLoanTableFormatted = formatRecordsAsTable($userBooksOnLoan, $headerRow, "books", "id");
+	echo $userBooksOnLoanTableFormatted
+?>
