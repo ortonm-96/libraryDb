8f8cc04984870cac29cbeb8c69d49485ceeac37f
ortonm-96 <molly.orton.96@gmail.com>
2021-07-01 14:24:56 +0100

    Reworked book page layout

--- recordTemplates/recordPage_booksTemplate.php
+++ recordTemplates/recordPage_booksTemplate.php
@@ -33,60 +33,64 @@
 		}
 
 ?>
-<p>Book page template test</p>
+<p class="pl-1">Book page template test</p>
 <img src=<?php echo "\"{$cover_image_filepath}\""; ?>>
-<form method="post" enctype="multipart/form-data">
+<form class="border mb-2 p-1" method="post" enctype="multipart/form-data">
 
-	<div class="form-group d-none">
-		<label for="id">ID</label>
+	<div class="input-group d-none">
+		<span class="input-group-text">ID</span>
 		<input required type="number" name="id" id="id" class="form-control" value=<?php echo "\"{$id}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="isbn">ISBN</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text">ISBN</span>
 		<input required type="number" name="isbn" id="isbn" class="form-control" value=<?php echo "\"{$isbn}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="name">Name</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text">Name</span>
 		<input type="text" name="name" id="name" class="form-control" value=<?php echo "\"{$name}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="description">Description</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text">Description</span>
 		<input type="text" name="description" id="description" class="form-control" value=<?php echo "\"{$description}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="loan_date">Loan Date</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text">Loan Date</span>
 		<input type="date" name="loan_date" id="loan_date" class="form-control" value=<?php echo "\"{$loan_date}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="due_date">Due Date</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text">Due Date</span>
 		<input disabled type="date" name="due_date" id="due_date" class="form-control" value=<?php echo "\"{$due_date}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="fileToUpload">Upload new cover image</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text">Upload new cover image</span>
 		<input type="file" name="fileToUpload" id="fileToUpload" class="form-control">
 	</div>
 
-	<div class="form-group">
-		<label for="loaned_by_user_id">Loaned By</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text">Loaned By</span>
 		<!-- Hide the actual input, because there's no reason for the user to have access to this.-->
 		<input type="hidden" name="loaned_by_user_id" id="loaned_by_user_id" class="form-control" value=<?php echo "\"{$loaned_by_user_id}\""; ?>></input>
 		<!-- Generate a link to the user's record page if applicable -->
-		<a href=<?php echo "{$userRecordLink}"; ?>><?php echo "{$userFullName}"; ?></a>
+		<!--<span class="input-group-text"><a href=<?php echo "{$userRecordLink}"; ?>><?php echo "{$userFullName}"; ?></a></span>-->
+		<a class="btn btn-light btn-outline-info" href=<?php echo "{$userRecordLink}"; ?>><?php echo "{$userFullName}"; ?></a>
 	</div>
 
-	<div class="form-group d-none">
-		<label for="cover_image_filepath">cover_image_filepath</label>
+	<div class="input-group d-none">
+		<span class="input-group-text">cover_image_filepath</span>
 		<input type="text" name="cover_image_filepath" id="cover_image_filepath" class="form-control" value=<?php echo "\"{$cover_image_filepath}\""; ?>></input>
 	</div>
 
-	<button permissionLevel="1" type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/updated_record.php">Update</button>
-	<button permissionLevel="1" type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/deleted_record.php">Delete</button>
-	<button type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/loanBook.php"<?php echo $loanDisabled; ?>>Loan book</button>
-	<button type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/returnBook.php"<?php echo $returnDisabled; ?>>Return book</button>
+	<div class="input-group mb-3">
+		<button permissionLevel="1" type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/updated_record.php">Update</button>
+		<button permissionLevel="1" type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/deleted_record.php">Delete</button>
+		<button type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/loanBook.php"<?php echo $loanDisabled; ?>>Loan book</button>
+		<button type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/returnBook.php"<?php echo $returnDisabled; ?>>Return book</button>
+	</div>
+
 </form>
