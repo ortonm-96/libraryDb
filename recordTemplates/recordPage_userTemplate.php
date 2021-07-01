af6807d07ca703ee092144cd43ca628130d6b39c
ortonm-96 <molly.orton.96@gmail.com>
2021-07-01 12:51:23 +0100

    Reworked user page layout

--- recordTemplates/recordPage_userTemplate.php
+++ recordTemplates/recordPage_userTemplate.php
@@ -1,7 +1,6 @@
 <?php
 require_once "{$_SERVER["DOCUMENT_ROOT"]}/libraryDb/functions/showtable_books.php";
 
-//echo $resultsFormatted
 $user_id = $queryResults[0]["user_id"] ?: "";
 $first_name = $queryResults[0]["first_name"] ?: "";
 $last_name = $queryResults[0]["last_name"] ?: "";
@@ -15,59 +14,61 @@ $permission_level_names = [
 ];
 
 ?>
-<p>User page - <?php echo $full_name; ?></p>
-<form method="post">
+<p class="pl-1">User page - <?php echo $full_name; ?></p>
+<form class="border mb-2 p-1" method="post">
 
-	<div permissionLevel="1" class="form-group d-none">
-		<label for="user_id">ID</label>
+	<div permissionLevel="1" class="input-group d-none">
+		<span class="input-group-text">ID</span>
 		<input required type="number" name="user_id" id="user_id" class="form-control" value=<?php echo "\"{$user_id}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="first_name">First Name</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text" for="first_name">First Name</span>
 		<input type="text" name="first_name" id="first_name" class="form-control" value=<?php echo "\"{$first_name}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="last_name">Last Name</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text" for="last_name">Last Name</span>
 		<input type="text" name="last_name" id="last_name" class="form-control" value=<?php echo "\"{$last_name}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="username">Username</label>
+	<div class="input-group mb-3">
+		<span class="input-group-text" for="username">Username</span>
 		<input type="text" name="username" id="username" class="form-control" value=<?php echo "\"{$username}\""; ?>></input>
 	</div>
 
-	<div class="form-group" permissionLevel="1">
-		<label for="permission_level">Current role - <?php echo $permission_level_names[$user_permission_level]; ?></label>
+	<div class="input-group mb-3" permissionLevel="1">
+		<span class="input-group-text" for="permission_level">Current role - <?php echo $permission_level_names[$user_permission_level]; ?></span>
 		<select class="form-select" name="permission_level" id="permission_level">
 			<option selected value="0">Set user role</option>
 			<option value="1">Staff</option>
 			<option value="3">User</option>
 		</select>
 	</div>
-
-	<button permissionLevel="1" type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/updated_user.php">Update</button>
-	<button permissionLevel="1" type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/deleted_user.php">Delete</button>
+	<div class="input-group mb-3" permissionLevel="1">
+		<button permissionLevel="1" type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/updated_user.php">Update</button>
+		<button permissionLevel="1" type="submit" role="button" class="btn btn-light" formaction="/libraryDb/post_handlers/deleted_user.php">Delete</button>
+	</div>
 
 </form>
 
-<form method="post" permissionLevel="1">
-	<div class="form-group d-none">
-		<label for="user_id">ID</label>
+<form class="border mb-2 p-1" method="post" permissionLevel="1">
+	<div class="input-group d-none">
+		<span class="input-group-text">ID</span>
 		<input required type="number" name="user_id" id="user_id" class="form-control" value=<?php echo "\"{$user_id}\""; ?>></input>
 	</div>
 
-	<div class="form-group">
-		<label for="new_password">New password</label>
+	<div class="input-group">
+		<span class="input-group-text">New password</span>
 		<input type="password" name="new_password" id="new_password">
+		<button role="button" class="btn btn-danger" type="submit" formaction="/libraryDb/post_handlers/change_user_password.php">Change password</button>
 	</div>
-
-	<button role="button" class="btn btn-danger" type="submit" formaction="/libraryDb/post_handlers/change_user_password.php">Change password</button>
+	
 </form>
 
 <!-- Display books on loan - construct a query similar to , then pass to table render function -->
-<p>Books on loan</p>
+<div class="border mb-2 p-1">
+	<p class="pl-1">Books on loan</p>
 <?php
 	$userBooksOnLoan = getBooks_loaned_byUser($user_id);
 	$headerRow = [
@@ -80,3 +81,4 @@ $permission_level_names = [
 	$userBooksOnLoanTableFormatted = formatRecordsAsTable($userBooksOnLoan, $headerRow, "books", "id");
 	echo $userBooksOnLoanTableFormatted
 ?>
+</div>
